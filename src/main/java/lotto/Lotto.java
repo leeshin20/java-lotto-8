f0c83008654070aeaa2c3a629421cf06c67d65c5
leeshin20 <leeshin20@naver.com>
2025-11-03 20:03:05 +0900

    feat: 로또 클래스 중복 번호 검사 로직 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -14,6 +14,11 @@ public class Lotto {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+
+        long uniqueCount = numbers.stream().distinct().count();
+        if (uniqueCount != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
     }
 
     // TODO: 추가 기능 구현
